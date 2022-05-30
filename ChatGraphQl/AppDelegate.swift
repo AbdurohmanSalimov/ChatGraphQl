43ab1109a23c31c135ba453b7043c3d76fee5781
AzizbekSalimov <azizbek.salimov.1995@mail.ru>
2022-05-30 18:30:37 +0500

    mavlono

--- ChatGraphQl/AppDelegate.swift
+++ ChatGraphQl/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,18 +18,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let vc = RegisterVC()
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
+        registerForPushNotifications()
         return true
     }
+    
     func application(
       _ application: UIApplication,
       didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
     ) {
       let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
       let token = tokenParts.joined()
-      print("Device Token: \(token)")
-        Cache.shared.registerToken(token: "\(token)")
+      print("🇷🇺Device Token: \(token)")
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
+      print("Failed to register: \(error)")
+    }
+    
+    func application(
+      _ application: UIApplication,
+      didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+      fetchCompletionHandler completionHandler:
+      @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+      guard let aps = userInfo["aps"] as? [String: AnyObject] else {
+        completionHandler(.failed)
+        return
+      }
+      
+//      if aps["content-available"] as? Int == 1 {
+//        let podcastStore = PodcastStore.sharedStore
+//        podcastStore.refreshItems { didLoadNewItems in
+//          completionHandler(didLoadNewItems ? .newData : .noData)
+//        }
+//      }
     }
 
+    func registerForPushNotifications() {
+      UNUserNotificationCenter.current()
+        .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, error in
+          guard let self = self else { return }
+          print("Permission granted: \(granted)")
+          
+          guard granted else { return }
+          
+//          let viewAction = UNNotificationAction(
+//            identifier: Identifiers.viewAction, title: "View",
+//            options: [.foreground])
+//
+//          let newsCategory = UNNotificationCategory(
+//            identifier: Identifiers.newsCategory, actions: [viewAction],
+//            intentIdentifiers: [], options: [])
+          
+//          UNUserNotificationCenter.current()
+//            .setNotificationCategories([newsCategory])
+          
+          self.getNotificationSettings()
+      }
+    }
+    
     func getNotificationSettings() {
       UNUserNotificationCenter.current().getNotificationSettings { settings in
         print("Notification settings: \(settings)")
@@ -38,7 +89,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
       }
     }
-
 
 }
 
